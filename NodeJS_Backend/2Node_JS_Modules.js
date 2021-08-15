79485dfe2bb0da1aff4cc048b770f2402f63df9e
Piyush Agrawal <piyush200205@gmail.com>
2021-08-15 21:07:03 +0530

    added 3JS

--- NodeJS_Backend/2Node_JS_Modules.js
+++ NodeJS_Backend/2Node_JS_Modules.js
@@ -1,2 +1,10 @@
 //https://nodejs.org/api/fs.html
 //these all functions and modules are to be seen and searched everytime to perform any specific work
+//it have some specific functions to make our work more easier 
+const fs = require("fs");
+let text = fs.readFileSync("2random.txt", "utf-8");
+text = text.replace("node", "Piyush");
+console.log("The content of the file is");
+console.log(text);
+console.log("Creating a new file");
+fs.writeFileSync("2piyush.txt", text);
